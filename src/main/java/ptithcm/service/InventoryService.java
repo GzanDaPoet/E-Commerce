4dfd795a677f90be029b75c72fbad0bf93c31bc3
GzanDaPoet <nguyentoan.13.9.2002@gmail.com>
2023-06-05 11:41:20 +0700

    Inventory list/details

--- src/main/java/ptithcm/service/InventoryService.java
+++ src/main/java/ptithcm/service/InventoryService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import ptithcm.dao.inventory.InventoryDao;
 import ptithcm.dao.user.UserDao;
+import ptithcm.model.inventory.InventoryReceiving;
+import ptithcm.model.inventory.InventoryReceivingDetails;
 import ptithcm.model.inventory.StatusReceiving;
 import ptithcm.model.user.User;
 
@@ -29,4 +31,23 @@ public class InventoryService {
 		return userDao.getUserById(id);
 	}
 	
+	public List<InventoryReceiving> getAllInventoryReceivings(){
+		List<InventoryReceiving> listStatus = inventoryDao.getAllInventoryReceivings();
+		if (listStatus == null) {
+			return null;
+		}
+		return listStatus;
+	}
+	
+	public List<InventoryReceivingDetails> listInventoryReceivingDetails(int id){
+		List<InventoryReceivingDetails> listDetails = inventoryDao.getDetails(id);
+		if (listDetails == null) {
+			return null;
+		}
+		return listDetails;
+	}
+	
+	public InventoryReceiving getInventoryReceivingById(int id) {
+		return inventoryDao.getInventoryReceivingById(id);
+	}
 }
